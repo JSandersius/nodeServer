5e16da97ae0651c0b0bac19ef739b4e7c340aece
Julian Sandersius <sandersiusjulian@gmail.com>
2018-05-09 11:23:13 -0700

    built server hello world

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 // This will create a server and listen for any kind of HTTP request on 
 // URL http://127.0.0.1:8000/; when one is received, it will send a plain-text response "Hello World".
+
+// Load HTTP module
+var http = require("http");
+
+// Create HTTP server and listen on port 8000 for requests
+http.createServer(function(request, response) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body "Hello World"
+   response.end('Hello World\n');
+}).listen(8000);
+
+// Print URL for accessing server
+console.log('Server running at http://127.0.0.1:8000/');
