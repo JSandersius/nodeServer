7e0feda101d8f38d57ec406b6172f0337d21c7b0
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-27 13:55:58 -0700

    server running

--- test-node/hello.js
+++ test-node/hello.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
 
     // Send the response body "Hello World"
     response.end('Hello World\n');
-}).listen(8000);
+}).listen(3000);
 
 // Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/');
+console.log('Server running at http://127.0.0.1:3000/');
