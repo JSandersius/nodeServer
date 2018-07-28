adc4d4a8ed02c64f2027d0bb0b822162c2208654
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-27 18:00:12 -0700

    notes

--- express-test/app.js
+++ express-test/app.js
@@ -4,12 +4,23 @@ var express = require('express');
 //routing HTTP requests, configuring middleware, rendering HTML
 //views, registering a template engine, and modifying application
 //settings that control how the application behaves
-
 var app = express();
 
+
+//The middle part of the code (the three lines starting with app.get)
+//shows a route definition. The app.get() method specifies a callback 
+//function that will be invoked whenever there is an HTTP GET request 
+//with a path ('/') relative to the site root. The callback function 
+//takes a request and a response object as arguments, and simply
+//calls send() on the response to return the string "Hello World!"
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+
+
+//The final block starts up the server on port '3000' and prints a log
+//comment to the console. With the server running, you could go to 
+//localhost:3000 in your browser to see the example response returned.
 
 app.listen(3003, function () {
     console.log('Example app listening on port 3000!');
