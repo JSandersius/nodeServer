29ee3d418d722ae8331e0cd43a47d640a178bb45
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-27 17:26:22 -0700

    added notes

--- express-test/app.js
+++ express-test/app.js
@@ -1,4 +1,10 @@
+//these first two lines import express and create an express app
 var express = require('express');
+//This object, which is traditionally named app, has methods for 
+//routing HTTP requests, configuring middleware, rendering HTML
+//views, registering a template engine, and modifying application
+//settings that control how the application behaves
+
 var app = express();
 
 app.get('/', function (req, res) {
