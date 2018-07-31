091e792841d24f4af55260ef3812fe2ebfd01fb4
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-31 10:06:46 -0700

    server running

--- 0b-SetUpEnviro/simpNodeServ/server.js
+++ 0b-SetUpEnviro/simpNodeServ/server.js
@@ -1,18 +1,20 @@
+
 //Load Http module 
-const http = require("http");
-const hostname = "127.0.0.1";
+const http = require("http"); //import http module 
+const hostname = '127.0.0.1';
 const port = 3000;
 
 //create HTTP server and  listen on port 300 for requests 
 const server = http.createServer((req, res) => {
 
+    //the script then prints a message about what browser url you can use to test the server. 
     //set the response HTTP header with HTTP status and Content type 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Fuuuuuuckk\n');
+    res.end('Hello World\n');
 });
 
 //listen for request on port 3000, and as a callback function have the port location on logged 
 server.listen(port, hostname, () => {
-    console.log('Server running at http://${hostname}:{port}/');
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
