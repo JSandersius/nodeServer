443e171f8adc0de9871c5126ec49b1c2adcff7fe
Julian Sandersius <sandersiusjulian@gmail.com>
2018-07-31 18:31:59 -0700

    rename

--- 2-SkeletonSite/express-locallibrary-tutorial/notes/3viewNotes.js
+++ 2-SkeletonSite/express-locallibrary-tutorial/notes/3viewNotes.js
@@ -14,9 +14,13 @@ block content
 h1 = title
 p Welcome to #{ title }
 
+//Challenge yourself
+//Create a new route in /routes/users.js that will display the text "You're so cool" at URL /users/cool/. Test it by running the server and visiting http://localhost:3000/users/cool/ in your browser
 
+//Summary
+//You have now created a skeleton website project for the Local Library and verified that it runs using node. Most important, you also understand how the project is structured, so you have a good idea where we need to make changes to add routes and views for our local library.
 
-
+//Next we'll start modifying the skeleton so that works as a library website.
 
 
 
