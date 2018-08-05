070552759716c8128eab486a0a61bb494a722081
Julian Sandersius <sandersiusjulian@gmail.com>
2018-08-05 16:39:15 -0700

    changing db

--- 3-DatabaseMongo/express-locallibrary/app.js
+++ 3-DatabaseMongo/express-locallibrary/app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://AndJulianIsHisNamo:AnaBanana1994@ds163781.mlab.com:63781/local_library';
+var mongoDB = 'mongodb://JulianSandersius:AnaBanana1994@ds213612.mlab.com:13612/library';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
--- 3-DatabaseMongo/express-locallibrary/models/genre.js
+++ 3-DatabaseMongo/express-locallibrary/models/genre.js
@@ -1,7 +1,3 @@
-
-
-
-
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
